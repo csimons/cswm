8b4be06d13ddbfc9252c440575e3e16d989b68ed
Chris Simons <csimons@users.noreply.github.com>
2012-07-13 20:47:26 -0400

    Readded J/K window navigation, zooming, client-kill commands.

--- config.h
+++ config.h
@@ -20,19 +20,20 @@ static Key key[] = { \
    /* modifier          key         function    argument */ \
     { MODKEY|ShiftMask, XK_t,       spawn,      { .cmd = CMD_TERMINAL } }, \
     { MODKEY|ShiftMask, XK_w,       spawn,      { .cmd = CMD_BROWSER  } }, \
+    { MODKEY,           XK_j,       focusnext,  { 0 } }, \
+    { MODKEY,           XK_k,       focusprev,  { 0 } }, \
     { MODKEY,           XK_Tab,     focusnext,  { 0 } }, \
+    { MODKEY,           XK_Return,  zoom,       { 0 } }, \
+    { MODKEY|ShiftMask, XK_c,       killclient, { 0 } }, \
     { MODKEY|ShiftMask, XK_q,       quit,       { 0 } }, \
 };
     /*
-    { MODKEY,           XK_k,       focusprev,    { 0 } }, \
     { MODKEY,           XK_a,       attach,       { 0 } }, \
     { MODKEY,           XK_d,       detach,       { 0 } }, \
-    { MODKEY,           XK_Return,  zoom,         { 0 } }, \
     { MODKEY,           XK_g,       resizemaster, { .i = 15 } }, \
     { MODKEY,           XK_s,       resizemaster, { .i = -15 } }, \
     { MODKEY,           XK_i,       incnmaster,   { .i = 1 } }, \
     { MODKEY,           XK_r,       incnmaster,   { .i = -1 } }, \
-    { MODKEY|ShiftMask, XK_c,       killclient,   { 0 } }, \
     { MODKEY,           XK_space,   toggleview,   { 0 } }, \
     { MODKEY|ShiftMask, XK_space,   togglefloat,  { 0 } }, \
     */
